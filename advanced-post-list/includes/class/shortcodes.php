352e5556e1cc20075b741e70443700aa9d795aa3
EkoJr <ekojr1337@gmail.com>
2016-12-28 04:42:10 -0800

    Added Shortcode Class
    Added a file that contains both Shortcode & Internal Shortcode Classes.
    Initial Design.

---
+++ advanced-post-list/includes/class/shortcodes.php
@@ -0,0 +1,17 @@
+<?php
+class APL_Shortcodes
+{
+    
+}
+class APL_InternalShortcodes
+{
+    public function __construct()
+    {
+        
+    }
+    public function __destruct() 
+    {
+    }
+            
+}
+?>
